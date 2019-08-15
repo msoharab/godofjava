67cf6342ffa1a6484478b55baf6b0fe16d5ee5ff
msoharab <soharab.zuber@gmail.com>
2019-08-16 00:27:53 +0530

    Added 7.25.0 type1 and type2 with eclipse jbpm plugin 6.0.0 & runtime and dependency of 7.25.0...

---
+++ jbpm7.25.0-demo/SearchSort-7.25.0-type2/src/main/java/com/sample/ProcessMain.java
@@ -0,0 +1,92 @@
+package com.sample;
+
+/*
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.manager.RuntimeEngine;
+import org.kie.api.runtime.manager.RuntimeEnvironmentBuilder;
+import org.kie.api.runtime.manager.RuntimeManager;
+import org.kie.api.runtime.manager.RuntimeManagerFactory;
+import org.kie.api.task.TaskService;
+import org.kie.api.task.model.TaskSummary;
+import org.kie.test.util.db.PersistenceUtil;
+
+public class ProcessMain {
+
+	public static void main(String[] args) {
+
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks.getKieClasspathContainer();
+		KieBase kbase = kContainer.getKieBase("kbase");
+
+		RuntimeManager manager = createRuntimeManager(kbase);
+		RuntimeEngine engine = manager.getRuntimeEngine(null);
+		KieSession ksession = engine.getKieSession();
+		TaskService taskService = engine.getTaskService();
+
+		ksession.startProcess("com.sample.bpmn.hello");
+
+		// let john execute Task 1
+		List<TaskSummary> list = taskService.getTasksAssignedAsPotentialOwner("john", "en-UK");
+		TaskSummary task = list.get(0);
+		System.out.println("John is executing task " + task.getName());
+		taskService.start(task.getId(), "john");
+		taskService.complete(task.getId(), "john", null);
+
+		// let mary execute Task 2
+		list = taskService.getTasksAssignedAsPotentialOwner("mary", "en-UK");
+		task = list.get(0);
+		System.out.println("Mary is executing task " + task.getName());
+		taskService.start(task.getId(), "mary");
+		taskService.complete(task.getId(), "mary", null);
+
+		manager.disposeRuntimeEngine(engine);
+		System.exit(0);
+	}
+
+	private static RuntimeManager createRuntimeManager(KieBase kbase) {
+		Properties properties = new Properties();
+		properties.put("driverClassName", "org.h2.Driver");
+		properties.put("className", "org.h2.jdbcx.JdbcDataSource");
+		properties.put("user", "sa");
+		properties.put("password", "");
+		properties.put("url", "jdbc:h2:tcp://localhost/~/jbpm-db");
+		properties.put("datasourceName", "jdbc/jbpm-ds");
+		PersistenceUtil.setupPoolingDataSource(properties);
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("org.jbpm.persistence.jpa");
+		RuntimeEnvironmentBuilder builder = RuntimeEnvironmentBuilder.Factory.get()
+			.newDefaultBuilder().entityManagerFactory(emf)
+			.knowledgeBase(kbase);
+		return RuntimeManagerFactory.Factory.get()
+			.newSingletonRuntimeManager(builder.get(), "com.sample:example:1.0");
+	}
+
+}
